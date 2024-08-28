e927fd0a2a120cb6161d1a2dd18f31511febd335
WizkWong <chijianwong7@gmail.com>
2024-08-28 17:24:42 +0800

    change loading UI

--- src/components/CustomError.tsx
+++ src/components/CustomError.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CustomError = ({ errorMsg } : { errorMsg : string }) => {
   return (
     <View className="mx-5 my-2">
-        <Text className="text-lg font-medium">{errorMsg}</Text>
+      <Text className="text-lg font-medium">{errorMsg}</Text>
     </View>
   )
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 import React from 'react'
 
 const Loading = () => {
   return (
-    <View className="mx-5 my-2">
-        <Text className="text-lg font-medium">Loading...</Text>
+    <View className="flex-1 mx-5 my-2 justify-center items-center">
+      <ActivityIndicator className='mb-16' animating={true} size={64} color="blue" />
     </View>
   )
 }
