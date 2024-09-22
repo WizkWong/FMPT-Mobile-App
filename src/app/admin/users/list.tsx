eb9ee7d58a4bccd34ed2773bd60431965007714d
WizkWong <chijianwong7@gmail.com>
2024-09-23 00:31:47 +0800

    abstract UserList into component

--- src/app/admin/users/list.tsx
+++ src/app/admin/users/list.tsx
@@ -1,15 +1,10 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { View, FlatList, Pressable } from "react-native";
-import { getUserByFilter } from "../../../services/UserService";
+import { Pressable } from "react-native";
 import { router, useNavigation } from "expo-router";
-import { User } from "../../../types/user";
 import { useEffect } from "react";
 import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
-import { ActivityIndicator, Avatar, Card, Icon } from "react-native-paper";
 import useUtilityQuery from "../../../hooks/useUtilityQuery";
-import CustomError from "../../../components/CustomError";
 import useSearchBar from "../../../hooks/useSearchBar";
-import { capitalizedCase } from "../../../utils/utility";
+import UserList from "../../../components/user/UserList";
 
 const UserListPage = () => {
   const navigation = useNavigation();
@@ -41,76 +36,12 @@ const UserListPage = () => {
     });
   }, []);
 
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-  } = useInfiniteQuery({
-    queryKey: queryKey,
-    queryFn: ({ pageParam }) => getUserByFilter(pageParam, searchText),
-    initialPageParam: 0,
-    staleTime: Infinity,
-    getNextPageParam: (lastPage, pages, lastPageParam) =>
-      lastPage.data.hasNext ? lastPageParam + 1 : null,
-  });
-
-  const fetchMore = () => {
-    if (hasNextPage) {
-      fetchNextPage();
-    }
-  };
-
-  const renderItem = ({ item }: { item: User }) => {
-    return (
-      <Card
-        className="mx-4 my-2 pr-3"
-        onPress={() => router.push(`/admin/users/${item.id}`)}
-      >
-        <Card.Title
-          className="py-2"
-          title={item.username}
-          subtitle={`Role: ${capitalizedCase(item.role)}\nDepartment: ${
-            item.department
-          }`}
-          subtitleNumberOfLines={2}
-          left={(props) =>
-            item.image ? (
-              <Avatar.Image
-                {...props}
-                size={45}
-                source={{ uri: `data:image/jpg;base64,${item.image}` }}
-              />
-            ) : (
-              <Avatar.Icon {...props} className="bg-amber-500" size={45} color="white" icon="account" />
-            )
-          }
-          right={(props) => <Icon {...props} source="chevron-right" />}
-        />
-      </Card>
-    );
-  };
-
   return (
-    <View className="flex flex-col justify-center my-1">
-      <FlatList
-        data={data?.pages.flatMap((d) => d.data.userList)}
-        renderItem={renderItem}
-        ListEmptyComponent={() => (
-          <CustomError errorMsg={error?.message ?? "No results of Users"} />
-        )}
-        keyExtractor={(item) => item.id.toString()}
-        onRefresh={refresh}
-        refreshing={isFetching}
-        onEndReached={fetchMore}
-        onEndReachedThreshold={0}
-        ListFooterComponent={
-          <ActivityIndicator animating={isFetchingNextPage} />
-        }
-      />
-    </View>
+    <UserList
+      searchText={searchText}
+      componentOnPress={(user) => router.push(`/admin/users/${user.id}`)}
+      refresh={refresh}
+    />
   );
 };
 
---
+++ src/components/user/UserList.tsx
@@ -0,0 +1,104 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { View, FlatList } from "react-native";
+import { getUserByFilter } from "../../services/UserService";
+import { User } from "../../types/user";
+import { ActivityIndicator, Avatar, Card, Icon } from "react-native-paper";
+import useUtilityQuery from "../../hooks/useUtilityQuery";
+import CustomError from "../../components/CustomError";
+import { capitalizedCase } from "../../utils/utility";
+
+const UserList = ({
+  searchText,
+  componentOnPress,
+  refresh,
+  filterByDepartment = "",
+}: {
+  searchText: string;
+  componentOnPress?: (user: User) => void;
+  refresh?: () => void;
+  filterByDepartment?: string;
+}) => {
+  const queryKey = ["fetchUserList"];
+  const utilityQuery = useUtilityQuery();
+  refresh = refresh ?? (() => utilityQuery.resetInfiniteQueryPagination(queryKey));
+
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: queryKey,
+    queryFn: ({ pageParam }) => getUserByFilter(pageParam, searchText, filterByDepartment),
+    initialPageParam: 0,
+    staleTime: Infinity,
+    getNextPageParam: (lastPage, pages, lastPageParam) =>
+      lastPage.data.hasNext ? lastPageParam + 1 : null,
+  });
+
+  const fetchMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const renderItem = ({ item }: { item: User }) => {
+    return (
+      <Card
+        className="mx-4 my-2 pr-3"
+        onPress={() => componentOnPress(item)}
+      >
+        <Card.Title
+          className="py-2"
+          title={item.username}
+          subtitle={`Role: ${capitalizedCase(item.role)}\nDepartment: ${
+            item.department
+          }`}
+          subtitleNumberOfLines={2}
+          left={(props) =>
+            item.image ? (
+              <Avatar.Image
+                {...props}
+                size={45}
+                source={{ uri: `data:image/jpg;base64,${item.image}` }}
+              />
+            ) : (
+              <Avatar.Icon
+                {...props}
+                className="bg-amber-500"
+                size={45}
+                color="white"
+                icon="account"
+              />
+            )
+          }
+          right={(props) => <Icon {...props} source="chevron-right" />}
+        />
+      </Card>
+    );
+  };
+
+  return (
+    <View className="flex flex-col justify-center my-1">
+      <FlatList
+        data={data?.pages.flatMap((d) => d.data.userList)}
+        renderItem={renderItem}
+        ListEmptyComponent={() => (
+          <CustomError errorMsg={error?.message ?? "No results of Users"} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        onRefresh={refresh}
+        refreshing={isFetching}
+        onEndReached={fetchMore}
+        onEndReachedThreshold={0}
+        ListFooterComponent={
+          <ActivityIndicator animating={isFetchingNextPage} />
+        }
+      />
+    </View>
+  );
+};
+
+export default UserList;
